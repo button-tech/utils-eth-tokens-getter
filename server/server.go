cd43556345c871643e7933398205037b2b4c7199
Nick Kozlov <nk@buttonwallet.com>
2019-05-21 16:08:13 -0400

    auto commit

--- server/server.go
+++ server/server.go
@@ -12,8 +12,8 @@ type UsersAndToken struct {
 }
 
 type UserBalance struct {
-	User              string `json:"user"`
-	TokenBalanceGroup []TokenAndBalance
+	User              string            `json:"user"`
+	TokenBalanceGroup []TokenAndBalance `json:"token_balance_group"`
 }
 
 type TokenAndBalance struct {
@@ -30,19 +30,19 @@ func LookForTokens(c *gin.Context) {
 	}
 
 	contractAnswer, err := contractWrapper.RequestBalancesForUsersOnContract(reqData.Users, reqData.Tokens)
-	contractAnswerLen := len(contractAnswer)
 	amountOfUsers := len(reqData.Users)
 	amountOfTokens := len(reqData.Tokens)
-	upBorder := contractAnswerLen/ amountOfUsers
 
 	var userBalances = make([]UserBalance, 0)
-	for i := 0; i <= amountOfUsers; i++ {
+	for i := 0; i < amountOfUsers; i++ {
 		var userBalance UserBalance
 		var tokenBalanceGroup = make([]TokenAndBalance, 0)
-		for j := 0; j <= amountOfTokens; j++ {
+		var lhs = amountOfTokens * i
+		var rhs = lhs + amountOfTokens
+		for j := lhs; j < rhs; j++ {
 			var tokenBalance TokenAndBalance
-			tokenBalance.Balance = contractAnswer[]
-			tokenBalance.TokenAddress = reqData.Tokens[i]
+			tokenBalance.Balance = contractAnswer[j]
+			tokenBalance.TokenAddress = reqData.Tokens[j-lhs]
 			tokenBalanceGroup = append(tokenBalanceGroup, tokenBalance)
 		}
 		userBalance.User = reqData.Users[i]
