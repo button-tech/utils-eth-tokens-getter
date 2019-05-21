e107edd106fb9ccd8507090a1fe3261aa7223ca6
Kirill <krboktv@gmail.com>
2019-05-21 05:20:14 -0400

    configs

---
+++ contract/truffle-config.js
@@ -0,0 +1,35 @@
+const env = process.env;
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+module.exports = {
+    compilers: {
+        solc: {
+            version: "0.5.2",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            }
+        }
+    },
+    networks: {
+        testrpc: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*', // eslint-disable-line camelcase
+        },
+        ganache: {
+            host: 'localhost',
+            port: 8545,
+            skipDryRun: true,
+            network_id: '*', // eslint-disable-line camelcase
+        },
+        rinkeby: {
+            provider: new HDWalletProvider(env.MNEMONIC, "https://rinkeby.infura.io/", 0, 10),
+            skipDryRun: true,
+            network_id: 4
+        }
+    },
+};
